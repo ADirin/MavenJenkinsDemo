64c057e79a711e131cb427c0708d84f48e7d525f
Amir Dirin <33372535+ADirin@users.noreply.github.com>
2023-09-18 10:29:34 +0300

    Add files via upload

---
+++ src/main/java/mavenTest_metropolia_fi/Calculator.java
@@ -0,0 +1,20 @@
+package mavenTest_metropolia_fi;
+
+public class Calculator {
+
+	public static int sum(int a, int b ) {
+		
+		return a+b;
+	}
+	
+public double product(double a, double b ) {
+		
+		return a*b;
+	}
+	
+public double subtract(double a, double b ) {
+	
+	return a-b;
+}
+	
+}
---
+++ src/test/java/mavenTest_metropolia_fi/CalculatorTest.java
@@ -0,0 +1,21 @@
+package mavenTest_metropolia_fi;
+
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+
+
+
+public class CalculatorTest {
+	
+	@Test
+	public void sumTest() {
+		assertEquals(1, 5.0,Calculator.sum(2,3));
+		
+		
+	}
+	
+
+}
